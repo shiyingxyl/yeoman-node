23d043ff0e3d8279a47d6d1ca44de1b01c1db0b7
yulong.xu <xuyulong@inspur.com>
2016-11-22 10:16:22 +0800

    fix bug

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ import assert from 'assert';
 import yeomanNode from '../lib';
 
 describe('yeoman-node', function () {
-  it('should have unit test!', function () {
+  it('should have unit test!1', function () {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
 });
