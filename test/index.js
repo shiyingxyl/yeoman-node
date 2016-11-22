9906e61610641fd5eecad76d82b684ec94bd9963
yulong.xu <xuyulong@inspur.com>
2016-11-22 10:18:38 +0800

    ddd

--- test/index.js
+++ test/index.js
@@ -3,6 +3,6 @@ import yeomanNode from '../lib';
 
 describe('yeoman-node', function () {
   it('should have unit test!1', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
+    assert(true, 'we expected this package author to add actual unit tests.');
   });
 });
